e5e82e5a8cfe2f3bf142df28b15e936e489d58b4
Ashley L Hill <ashleahhill@gmail.com>
2020-11-19 00:43:03 -0500

    feat: planck v6

--- keyboards/planck/keymaps/ashleahhill/keymap.c
+++ keyboards/planck/keymaps/ashleahhill/keymap.c
@@ -27,6 +27,7 @@ enum planck_layers {
   _QWERTY,
   _LOWER,
   _RAISE,
+  _PLOVER,
   _ADJUST,
   _FUNC,
   _RESET,
@@ -49,38 +50,38 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_planck_grid( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      LALT_T(KC_TAB),KC_Q, KC_W,   KC_E,      KC_R,   KC_T,                    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,      KC_BSPC,\
-      LCTL(KC_CAPS), KC_A, KC_S,   KC_D,      KC_F,   KC_G,                    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,   KCL_QUOTL,\
-      KC_LSFT,       KC_Z, KC_X,   KC_C,      KC_V,   KC_B,                    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RSFT,\
-      BACKLIT,       KC_NO, KC_LCTL, KC_LGUI, LOWER,  KC_SPC,                  KC_SPC  RAISE,   KC_RGUI,  KC_RCTL,  KC_NO  KC_ENT
+      LALT_T(KC_TAB), KC_Q,  KC_W,    KC_E,     KC_R,   KC_T,                    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,\
+      LCTL(KC_CAPS),  KC_A,  KC_S,    KC_D,     KC_F,   KC_G,                    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,\
+      KC_LSFT,        KC_Z,  KC_X,    KC_C,     KC_V,   KC_B,                    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,\
+      BACKLIT,        KC_NO, KC_LCTL, KC_LGUI,  LOWER,  KC_SPC,                  KC_SPC, RAISE,   KC_RGUI, KC_RCTL, KC_NO,   KC_ENT\
    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
   ),
 
   [_LOWER] = LAYOUT_planck_grid( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                           KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,\
-      KC_CAPS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                     KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,\
-      KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT,\
-      XXXXXXX, XXXXXXX, KC_LCTL, KC_LGUI, LOWER,        MO(_FUNC),    MO(_FUNC),     RAISE, KC_RGUI,  KC_RCTL,XXXXXXX, XXXXXXX,
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,  KC_5,                           KC_6,      KC_7,     KC_8,    KC_9,    KC_0,    KC_DEL,\
+      KC_CAPS, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,                          KC_MINS,   KC_EQL,   KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,\
+      KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,                          KC_NO,     KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_RSFT,\
+      XXXXXXX, XXXXXXX, KC_LCTL, KC_LGUI, LOWER, MO(_FUNC),                      MO(_FUNC), RAISE,    KC_RGUI, KC_RCTL, XXXXXXX, XXXXXXX\
    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
   ),
 
   [_RAISE] = LAYOUT_planck_grid( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_LALT, KC_1, KC_2, KC_3, KC_4, KC_5,                          KC_6, KC_7, KC_8, KC_9, KC_0, KC_RALT,\
-      KC_LGUI, KC_NO, KC_NO, KC_INS, KC_HOME, KC_PGUP,                KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_LGUI,\
-      KC_LSFT, KC_NO, KC_NO, KC_DEL, KC_END, KC_PGDN,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT,\
-           XXXXXXX, XXXXXXX, KC_LCTL, KC_LGUI, LOWER,   MO(_FUNC), MO(_FUNC),     RAISE, KC_RGUI,  KC_RCTL,XXXXXXX, XXXXXXX,
+      KC_LALT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6, KC_7, KC_8, KC_9, KC_0, KC_RALT,\
+      KC_LGUI, KC_NO,   KC_NO,   KC_INS,  KC_HOME, KC_PGUP,                      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_LGUI,\
+      KC_LSFT, KC_NO,   KC_NO,   KC_DEL,  KC_END,  KC_PGDN,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT,\
+      XXXXXXX, XXXXXXX, KC_LCTL, KC_LGUI, LOWER, MO(_FUNC),                      MO(_FUNC), RAISE, KC_RGUI, KC_RCTL, XXXXXXX, XXXXXXX\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
   ),
 
   [_FUNC] = LAYOUT_planck_grid( \
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                       KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,\
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                       KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,\
-      LCTL(LGUI(KC_LEFT)), LCTL(LGUI(KC_RIGHT)), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_MPLY, KC_MUTE, KC_VOLD, KC_VOLU, \
-           XXXXXXX, XXXXXXX, KC_LCTL, KC_LGUI, LOWER,        _______, _______,     RAISE, KC_RGUI,  KC_RCTL,XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+  //,-----------------------------------------------------.                                  ,-----------------------------------------------------.
+      KC_F1,   KC_F2,                KC_F3,               KC_F4,   KC_F5, KC_F6,              KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,\
+      KC_NO,   KC_NO,                KC_NO,               KC_NO,   KC_NO, KC_NO,              KC_NO,   KC_NO,   KC_NO,   KC_NO,   GUI_ON,  GUI_OFF,\
+      KC_NO,   KC_NO,                KC_NO,               KC_NO,   KC_NO, KC_NO,              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MNXT, KC_MPLY, \
+      XXXXXXX, LCTL(LGUI(KC_LEFT)), LCTL(LGUI(KC_RIGHT)), KC_LGUI, LOWER, _______,            _______, RAISE,   KC_RGUI, KC_VOLD, KC_VOLU, KC_MUTE\
+  //|--------+--------+--------+--------+--------+--------+--------|                |--------+--------+--------+--------+--------+--------+--------|
   ),
 
 /* Plover layer (http://opensteno.org)
@@ -113,19 +114,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_ADJUST] = LAYOUT_planck_grid(
-    _______, MO(_RESET),   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,   _______,  _______, _______,  _______,
-    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
-)
+  [_ADJUST] = LAYOUT_planck_grid(
+      _______, MO(_RESET), DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
+      _______, _______,    MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______,  _______, _______, _______,
+      _______, MUV_DE,     MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
+      _______, _______,    _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+  ),
 
   [_RESET] = LAYOUT_planck_grid( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      RESET, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO,                       TO(_QWERTY), KC_NO, KC_NO, KC_NO, KC_NO, DEBUG,\
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,\
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,\
-           XXXXXXX, XXXXXXX, KC_LCTL, KC_LGUI, LOWER, _______,      _______, RAISE, KC_RGUI,  KC_RCTL,XXXXXXX, XXXXXXX,
+      RESET,   KC_TRNS, KC_NO,   KC_NO,   KC_NO, KC_NO,        TO(_QWERTY), KC_NO, KC_NO,    KC_NO,   KC_NO,   DEBUG,\
+      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,        KC_NO,       KC_NO, KC_NO,    KC_NO,   KC_NO,   KC_NO,\
+      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,        KC_NO,       KC_NO, KC_NO,    KC_NO,   KC_NO,   KC_NO,\
+      XXXXXXX, XXXXXXX, KC_LCTL, KC_LGUI, LOWER, _______,      _______,     RAISE, KC_RGUI,  KC_RCTL, XXXXXXX, XXXXXXX\
 
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
   )
